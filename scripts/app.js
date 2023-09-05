6d98be4a3b2774fec3cf194d061729e0c5df44fe
mersad <mersad.up@gmail.com>
2023-09-05 15:18:16 +0330

    debug mobile menu

--- scripts/app.js
+++ scripts/app.js
@@ -4,27 +4,51 @@ let mobileMenu = $.querySelector('.mobile-menu');
 let closeMobileMenuBtn = $.querySelector('.close-btn');
 let nextSlideBtn = $.querySelector(".next-slide-btn");
 let prevSlideBtn = $.querySelector(".prev-slide-btn");
+let cover = $.querySelector(".cover");
+let HtmlElem = $.querySelector("html");
+let menuActive = false;
 
 
-
-
-function openCloseMenu(){
-    mobileMenuBtn.classList.toggle('menu-btn--active');
-    mobileMenu.classList.toggle('mobile-menu--open')
-}
-function CloseMenu(){
-    mobileMenu.classList.remove('mobile-menu--open');
-    mobileMenuBtn.classList.remove('menu-btn--active');
-}
 
 mobileMenuBtn.addEventListener("click", openCloseMenu);
 closeMobileMenuBtn.addEventListener("click", CloseMenu);
+cover.addEventListener("click", CloseMenu);
 nextSlideBtn.addEventListener("click",()=>{
     swiper.slidePrev();
 })
 prevSlideBtn.addEventListener("click",()=>{
     swiper.slideNext();
 })
+window.addEventListener("scroll",(e)=>{
+    if(menuActive == false){
+        return;
+    }
+    else{
+        scroll(0 , 0);
+    }
+})
+
+
+
+
+function openCloseMenu(){
+    mobileMenuBtn.classList.add('menu-btn--active');
+    mobileMenu.classList.add('mobile-menu--open');
+    cover.classList.add("cover--active");
+    HtmlElem.style.scrollBehavior = "unset";
+    menuActive = true;
+}
+function CloseMenu(){
+    mobileMenu.classList.remove('mobile-menu--open');
+    mobileMenuBtn.classList.remove('menu-btn--active');
+    cover.classList.remove("cover--active");
+    HtmlElem.style.scrollBehavior = "smooth";
+    menuActive = false;
+    console.log('close');
+}
+
+
+
 
 
 //  slider
